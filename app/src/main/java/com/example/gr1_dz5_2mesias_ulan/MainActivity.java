07dcfc5494a64e57b8ec9f16cceab8dcbcab3518
ulik312 <Ulik312@gmail.com>
2022-10-20 18:30:55 +0600

    Initial commit

---
+++ app/src/main/java/com/example/gr1_dz5_2mesias_ulan/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.gr1_dz5_2mesias_ulan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
